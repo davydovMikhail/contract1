0c70a72e2017b85e8be22660b9aea44aa2f12b08
davydovMikhail <davydov27977@gmail.com>
2022-02-14 13:44:02 +0700

    fix text in deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,7 @@
 async function main() {
-    // const [deployer] = await ethers.getSigners();
-
-    // console.log("Deploying contracts with the account:", deployer.address);
-
-    // console.log("Account balance:", (await deployer.getBalance()).toString());
-
     const Donat = await ethers.getContractFactory("Donat");
     const donat = await Donat.deploy();
-
-    console.log("Token address:", donat.address);
+    console.log("Contract address:", donat.address);
 }
   
 main()
